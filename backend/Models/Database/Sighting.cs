72e4bfab5252d53f674682416eada7787837c915
Palak Mittal <Palak.Mittal@softwire.com>
2022-06-28 15:07:00 +0100

    WW-12: merge species and sighting models

--- backend/Models/Database/Sighting.cs
+++ backend/Models/Database/Sighting.cs
@@ -5,21 +5,13 @@ namespace WhaleSpotting.Models.Database
     public class Sighting
     {
         public int Id { get; set; }
-
         public double Latitude { get; set; }
-
         public double Longitude { get; set; }
-
         public DateTime Date { get; set; }
-
         public string Description { get; set; }
-
         public string PhotoUrl { get; set; }
-
-        //public Species Species {get; set;}
-
-        public bool Approved { get; set; }
-
-        public bool Deleted { get; set; }
+        public Species Species { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
