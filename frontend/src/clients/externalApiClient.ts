81c5cefc94766c6f76e571ddd3904431ffa6b76d
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-06-29 16:08:18 +0100

    WS-20: adding more interfaces

--- frontend/src/clients/externalApiClient.ts
+++ frontend/src/clients/externalApiClient.ts
@@ -1,21 +1,44 @@
-export interface ListResponse<T> {
-  items: T[];
+export interface Sightings {
+  sightings: Sighting[];
+}
+
+export interface SpeciesList {
+  species: Species[];
+}
+
+export interface Locations {
+  locations: Location[];
+}
+
+export interface Location {
+  id: number;
+  latitude: number; // ranges fron -90.0 to 90.0
+  longitude: number; // ranges from -180.0 to 180.0
+  name: string; // the common name of the location (if it has one!)
+  description: string;
+  sightings: Sighting[]; // a list of all sightings at this location (those Sighting objects do NOT include the location field)
+}
+
+export interface Species {
+  id: number;
+  name: string;
+  latinName: string;
+  photoUrl: string;
+  description: string;
+  endangeredStatus: string;
+  sightings: Sighting[];
 }
 
 export interface Sighting {
-  location: {
-    latitude: number;
-    longitude: number;
-  };
-  species: {
-    id: number;
-    name: string;
-  };
-  date: Date;
+  id: number;
+  location: Location;
+  species: Species[];
+  date: string;
   photoUrl: string;
+  email: string;
 }
 
-export async function fetchSightings(): Promise<ListResponse<Sighting>> {
+export async function fetchSightings(): Promise<Sightings> {
   const response = await fetch(
     `http://whale-spotting-external-api.herokuapp.com/api/sightings`
   );
