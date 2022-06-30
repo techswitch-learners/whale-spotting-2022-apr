4dae668ca7a99798550d98a71012c5e47a8c0a77
Palak Mittal <Palak.Mittal@softwire.com>
2022-06-30 14:45:21 +0100

    WW-11: Working on delete sighting endpoint

--- backend/Repositories/SightingRepo.cs
+++ backend/Repositories/SightingRepo.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using WhaleSpotting.Models.Database;
 
@@ -7,6 +8,8 @@ namespace WhaleSpotting.Repositories
     public interface ISightingRepo
     {
         IEnumerable<Sighting> GetAllSightings();
+        Sighting GetById(int id);
+        void Delete(int id);
     }
 
     public class SightingRepo : ISightingRepo
@@ -24,5 +27,19 @@ namespace WhaleSpotting.Repositories
                 .Sightings
                 .Include(s => s.Species);
         }
+
+         public Sighting GetById(int id)
+        {
+            return _context
+                .Sightings
+                .Single(s => s.Id == id);
+        }
+         public void Delete(int id)
+        {
+            var sighting = GetById(id);
+            _context.Sightings.Remove(sighting);
+            _context.SaveChanges();
+        }
+
     }
 }
