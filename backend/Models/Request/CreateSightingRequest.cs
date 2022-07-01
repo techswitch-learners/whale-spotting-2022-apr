c650937c466386719910535164ac597eebffec50
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-07-01 10:17:55 +0100

    WS-06: fixing error message on createsightingrequest

--- backend/Models/Request/CreateSightingRequest.cs
+++ backend/Models/Request/CreateSightingRequest.cs
@@ -9,11 +9,11 @@ namespace WhaleSpotting.Models.Request
         public DateTime Date { get; set; }
 
         [Required]
-        [Range (-90,90, ErrorMessage = "Latitude must be between 0 and 1")]
+        [Range (-90,90, ErrorMessage = "Latitude must be between -90 and 90")]
         public double Latitude { get; set; }
 
         [Required]
-        [Range (-180,180, ErrorMessage = "Longitude must be between 0 and 1")]
+        [Range (-180,180, ErrorMessage = "Longitude must be between -180 and 180")]
         public double Longitude { get; set; }
 
         [Required]
