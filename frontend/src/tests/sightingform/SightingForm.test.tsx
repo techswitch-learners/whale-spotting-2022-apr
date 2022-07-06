7ceeb23cbb4275f7bbb1daa6ed4f6a08b664c9c1
Tim Leach <timleach635@gmail.com>
2022-07-06 12:38:05 +0100

    WS-14: Begin form submit test
    Begin writing the test for the CreateSightingForm that tests what
    happens when the HTML form is submitted

--- frontend/src/tests/sightingform/SightingForm.test.tsx
+++ frontend/src/tests/sightingform/SightingForm.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CreateSightingForm } from "../../components/createsighting/CreateSightingForm";
+import * as apiClient from "../../clients/internalApiClient";
 
 test("Should render without error", () => {
   render(<CreateSightingForm />);
@@ -21,4 +22,25 @@ test("When rendered, all form elements are present", () => {
   expect(descriptionInput).toBeInTheDocument();
   expect(photoInput).toBeInTheDocument();
   expect(speciesInput).toBeInTheDocument();
+});
+
+test("Submitting the form with values calls the API client with a NewSightingRequest created from the submitted values", () => {
+  render(<CreateSightingForm />);
+
+  const createSighting = jest
+    .spyOn(apiClient, "createSighting")
+    .mockImplementation(async () => {
+      console.log("Called createSighting()");
+    });
+
+  const dateInput = screen.getByLabelText(/date/i);
+  const latitudeInput = screen.getByLabelText(/latitude/i);
+  const longitudeInput = screen.getByLabelText(/longitude/i);
+  const descriptionInput = screen.getByLabelText(/description/i);
+  const photoInput = screen.getByLabelText(/photo/i);
+  const speciesInput = screen.getByLabelText(/species/i);
+
+  fireEvent.click(screen.getByText(/submit/i));
+
+  expect(createSighting).toBeCalled();
 });
