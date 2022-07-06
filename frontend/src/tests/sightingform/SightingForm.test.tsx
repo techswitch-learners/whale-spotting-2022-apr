0ae36ef35f93fe6becd48b15781bfbb691153811
Tim Leach <timleach635@gmail.com>
2022-07-06 11:21:09 +0100

    WS-14: Add form input test
    Add a test to the CreateSightingForm component that ensures all six
    expected form fields are present when the component is rendered

--- frontend/src/tests/sightingform/SightingForm.test.tsx
+++ frontend/src/tests/sightingform/SightingForm.test.tsx
@@ -1,16 +1,24 @@
 import { render, screen } from "@testing-library/react";
-import { createMemoryHistory } from "history";
-import { Router } from "react-router-dom";
 import { CreateSightingForm } from "../../components/createsighting/CreateSightingForm";
 
 test("Should render without error", () => {
-  const history = createMemoryHistory();
-  render(
-    <Router history={history}>
-      <CreateSightingForm />
-    </Router>
-  );
+  render(<CreateSightingForm />);
+});
 
-  const elements = screen.getAllByText(/Sightings/i);
-  expect(elements[0]).toBeInTheDocument();
+test("When rendered, all form elements are present", () => {
+  render(<CreateSightingForm />);
+
+  const dateInput = screen.getByLabelText(/date/i);
+  const latitudeInput = screen.getByLabelText(/latitude/i);
+  const longitudeInput = screen.getByLabelText(/longitude/i);
+  const descriptionInput = screen.getByLabelText(/description/i);
+  const photoInput = screen.getByLabelText(/photo/i);
+  const speciesInput = screen.getByLabelText(/species/i);
+
+  expect(dateInput).toBeInTheDocument();
+  expect(latitudeInput).toBeInTheDocument();
+  expect(longitudeInput).toBeInTheDocument();
+  expect(descriptionInput).toBeInTheDocument();
+  expect(photoInput).toBeInTheDocument();
+  expect(speciesInput).toBeInTheDocument();
 });
