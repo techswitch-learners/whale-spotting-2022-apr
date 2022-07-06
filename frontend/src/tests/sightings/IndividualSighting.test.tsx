d6261061874db553a0ccc51c5c17d01256b49772
Tim Leach <timleach635@gmail.com>
2022-07-06 15:12:41 +0100

    WS-15: Begin adding tests
    Begin adding tests to the component, ensuring that it calls the right
    API endpoints and displays the right info (WIP)

--- frontend/src/tests/sightings/IndividualSighting.test.tsx
+++ frontend/src/tests/sightings/IndividualSighting.test.tsx
@@ -2,9 +2,30 @@ import { render, screen } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
 import { IndividualSighting } from "../../components/individualSighting/individualSighting";
+import * as apiClient from "../../clients/internalApiClient";
 
-test("Should render without error", () => {
-  const history = createMemoryHistory();
+test('When rendered with a route, sends a fetch request to the "get sighting by ID" endpoint in the backend', () => {
+  const history = createMemoryHistory({ initialEntries: ["/sightings/1"] });
+
+  const fetchSightingById = jest
+    .spyOn(apiClient, "fetchSightingById")
+    .mockImplementation(async (id: number) => ({
+      id: id,
+      latitude: 0,
+      longitude: 0,
+      date: "2022-07-06",
+      description: "A sighting",
+      photoUrl: "",
+      species: {
+        id: 1,
+        name: "Blue whale",
+        latinName: "Blueus Whaleus",
+        endangeredStatus: "Not Endangered, I Hope",
+        imageUrl: "",
+        description: "A whale, coloured blue",
+      },
+      isApproved: true,
+    }));
 
   render(
     <Router history={history}>
@@ -12,6 +33,41 @@ test("Should render without error", () => {
     </Router>
   );
 
-  const elements = screen.getAllByText(/Whale spotting sighting/i);
-  expect(elements[0]).toBeInTheDocument();
+  expect(fetchSightingById).toBeCalled();
+});
+
+test("When rendered with a route, displays the information of the sighting with that ID", async () => {
+  const history = createMemoryHistory({ initialEntries: ["/sightings/1"] });
+
+  jest
+    .spyOn(apiClient, "fetchSightingById")
+    .mockImplementation(async (id: number) => ({
+      id: id,
+      latitude: 0,
+      longitude: 0,
+      date: "2022-07-06",
+      description: "A sighting",
+      photoUrl: "",
+      species: {
+        id: 1,
+        name: "Blue whale",
+        latinName: "Blueus Whaleus",
+        endangeredStatus: "Not Endangered, I Hope",
+        imageUrl: "",
+        description: "A whale, coloured blue",
+      },
+      isApproved: true,
+    }));
+
+  render(
+    <Router history={history}>
+      <IndividualSighting />
+    </Router>
+  );
+
+  expect(
+    await screen.getByText(/description: a sighting/i)
+  ).toBeInTheDocument();
+  expect(await screen.getByText(/blue whale/i)).toBeInTheDocument();
+  expect(await screen.getByText(/a whale, coloured blue/i)).toBeInTheDocument();
 });
