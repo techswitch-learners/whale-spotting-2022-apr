0f73394854ab419e4055020f3d03d3922d9ac9e9
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-07-07 15:08:09 +0100

    WS-37: Begin test set up

--- frontend/src/tests/homepage/Home.test.tsx
+++ frontend/src/tests/homepage/Home.test.tsx
@@ -1,8 +1,43 @@
 import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
 import { Home } from "../../components/homepage/Home";
+import { Router } from "react-router-dom";
+import { BinocularMarker } from "../../components/homepage/BinocularMarker";
+
+const testSighting = {
+  id: 1,
+  latitude: 54.682709,
+  longitude: -43.553958,
+  description: "Nice place",
+  date: "2021-10-21 00:00:00",
+  photoUrl:
+    "https://i0.wp.com/handluggageonly.co.uk/wp-content/uploads/2017/05/humpback-1209297_1920.jpg?w=1600&ssl=1",
+  species: {
+    id: 2,
+    name: "Beluga whale",
+    latinName: "Beluga whale",
+    endangeredStatus: "endangered",
+    imageUrl: "https://s.hdnux.com/photos/01/24/11/10/22057501/4/2400x0.jpg",
+    description:
+      "The orca or killer whale (Orcinus orca) is a toothed whale belonging to the oceanic dolphin family, of which it is the largest member. ",
+  },
+  isApproved: true,
+};
 
 test("renders whale spotting text to screen", () => {
   render(<Home />);
   const elements = screen.getAllByText(/whale spotting/i);
+  expect(elements[0]).toBeInTheDocument();
+});
+
+test("Should render with information about the sighting", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <BinocularMarker sighting={testSighting} />
+    </Router>
+  );
+
+  const elements = screen.getAllByText(/Beluga/i);
   expect(elements[0]).toBeInTheDocument();
 });
