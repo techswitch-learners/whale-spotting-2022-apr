06c8c6e2768db34b9fb5c22bc07032fb39e319f2
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-07-08 09:48:41 +0100

    WS-37: Add test for fetch request

--- frontend/src/tests/homepage/Home.test.tsx
+++ frontend/src/tests/homepage/Home.test.tsx
@@ -3,7 +3,7 @@ import { createMemoryHistory } from "history";
 import { Home } from "../../components/homepage/Home";
 import { Router } from "react-router-dom";
 import { BinocularMarker } from "../../components/homepage/BinocularMarker";
-import { SightingResponse } from "../../clients/internalApiClient";
+import { SightingListResponse } from "../../clients/internalApiClient";
 import * as apiClient from "../../clients/internalApiClient";
 
 test("renders whale spotting text to screen", () => {
@@ -18,57 +18,62 @@ test("renders map component to screen", () => {
   expect(map[0]).toBeInTheDocument();
 });
 
-// test('When rendered, sends a fetch request to the "get sighting by ID" endpoint in the backend', () => {
-//   const history = createMemoryHistory();
+test('When rendered, sends a fetch request to the "getSightings" endpoint in the backend', () => {
+  const history = createMemoryHistory();
 
-//   const sightingDummyData : SightingResponse[] = [{
-//       id: 1,
-//       latitude: 0,
-//       longitude: 0,
-//       date: "2022-07-06",
-//       description: "A sighting",
-//       photoUrl: "",
-//       species: {
-//         id: 1,
-//         name: "Blue whale",
-//         latinName: "Blueus Whaleus",
-//         endangeredStatus: "Not Endangered, I Hope",
-//         imageUrl: "",
-//         description: "A whale, coloured blue",
-//       },
-//       isApproved: true,
-//     }, {
-//       id: 2,
-//       latitude: 30,
-//       longitude: 50,
-//       date: "2022-07-06",
-//       description: "A sighting, very amazing",
-//       photoUrl: "",
-//       species: {
-//         id: 1,
-//         name: "Blue whale",
-//         latinName: "Blueus Whaleus",
-//         endangeredStatus: "Not Endangered, I Hope",
-//         imageUrl: "",
-//         description: "A whale, coloured blue",
-//       },
-//       isApproved: false,
-//     }]
+  const sightingDummyData: SightingListResponse = {
+    sightings: [
+      {
+        id: 1,
+        latitude: 0,
+        longitude: 0,
+        date: "2022-07-06",
+        description: "A sighting",
+        photoUrl: "",
+        species: {
+          id: 1,
+          name: "Blue whale",
+          latinName: "Blueus Whaleus",
+          endangeredStatus: "Not Endangered, I Hope",
+          imageUrl: "",
+          description: "A whale, coloured blue",
+        },
+        isApproved: true,
+      },
+      {
+        id: 2,
+        latitude: 30,
+        longitude: 50,
+        date: "2022-07-06",
+        description: "A sighting, very amazing",
+        photoUrl: "",
+        species: {
+          id: 1,
+          name: "Blue whale",
+          latinName: "Blueus Whaleus",
+          endangeredStatus: "Not Endangered, I Hope",
+          imageUrl: "",
+          description: "A whale, coloured blue",
+        },
+        isApproved: false,
+      },
+    ],
+  };
 
-//   const fetchSightings = jest
-//     .spyOn(apiClient, "fetchSightings")
-//     .mockImplementation(async () => (sightingDummyData));
+  const fetchSightings = jest
+    .spyOn(apiClient, "fetchSightings")
+    .mockImplementation(async () => sightingDummyData);
 
-//   act(() => {
-//     render(
-//       <Router history={history}>
-//         <Home />
-//       </Router>
-//     );
-//   });
+  act(() => {
+    render(
+      <Router history={history}>
+        <Home />
+      </Router>
+    );
+  });
 
-//   expect(fetchSightingById).toBeCalled();
-// });
+  expect(fetchSightings).toBeCalled();
+});
 
 // test("When rendered with a route, displays the information of the sighting with that ID", async () => {
 //   const history = createMemoryHistory({ initialEntries: ["/sightings/1"] });
