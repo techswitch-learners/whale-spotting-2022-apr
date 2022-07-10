04e96df80e83fecea67ccd2930f843cf449d1a6d
ljenchik <pelagenkolena@gmail.com>
2022-07-10 22:42:43 +0100

    WS-37: Added test for popup message, when clicked on binocular marker

--- frontend/src/tests/homepage/BinocularMarker.test.tsx
+++ frontend/src/tests/homepage/BinocularMarker.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import { createMemoryHistory } from "history";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { BinocularMarker } from "../../components/homepage/BinocularMarker";
+import { shallow } from "enzyme";
 
 const sighting = {
   id: 1,
@@ -42,4 +42,34 @@ test("When rendered, includes one binocular marker", async () => {
     </MapContainer>
   );
   await waitFor(() => expect(screen.getByRole("marker")).toBeInTheDocument());
+});
+
+it("displays popup when click on binocular marker", async () => {
+  {
+    render(
+      <MapContainer
+        center={[54.637581, -3.902469]}
+        zoom={5}
+        scrollWheelZoom={false}
+        style={{
+          height: "600px",
+          width: "600px",
+          maxHeight: "80vw",
+          maxWidth: "80vw",
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <BinocularMarker sighting={sighting} />
+      </MapContainer>
+    );
+    await waitFor(() => expect(screen.getByRole("marker")).toBeInTheDocument());
+    const marker = screen.getByRole("marker");
+    fireEvent.click(marker);
+    await waitFor(() =>
+      expect(screen.getByText(/A sighting with id 1/i)).toBeInTheDocument()
+    );
+  }
 });
