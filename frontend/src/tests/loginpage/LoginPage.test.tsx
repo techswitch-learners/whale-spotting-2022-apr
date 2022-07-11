d0be604b309dcf930f0e1345c66cbc3a02363b8a
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-07-11 12:20:30 +0100

    WS-39: Add tests to LoginPage and LoginForm

--- frontend/src/tests/loginpage/LoginPage.test.tsx
+++ frontend/src/tests/loginpage/LoginPage.test.tsx
@@ -3,10 +3,10 @@ import { LoginPage } from "../../components/login/LoginPage";
 
 test("input fields of the form can be found", () => {
   render(<LoginPage />);
-  const elements1 = screen.getAllByPlaceholderText(/username/i);
-  const elements2 = screen.getAllByPlaceholderText(/password/i);
-  const element3 = screen.getByRole("button", { name: /log in/i });
-  expect(elements1[0]).toBeInTheDocument();
-  expect(elements2[0]).toBeInTheDocument();
-  expect(element3).toBeInTheDocument();
+  const usernameField = screen.getByPlaceholderText(/username/i);
+  const passwordField = screen.getByPlaceholderText(/password/i);
+  const loginButton = screen.getByRole("button", { name: /log in/i });
+  expect(usernameField).toBeInTheDocument();
+  expect(passwordField).toBeInTheDocument();
+  expect(loginButton).toBeInTheDocument();
 });
