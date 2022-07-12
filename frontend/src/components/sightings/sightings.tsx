5b7c4508c2805304a39e3f3293297f59a414f9fd
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-07-12 14:25:11 +0100

    WS-38: Provide links only to internal sightings

--- frontend/src/components/sightings/sightings.tsx
+++ frontend/src/components/sightings/sightings.tsx
@@ -11,21 +11,40 @@ import { SightingCard } from "../sightingCard/sightingCard";
 import { compareDesc, parseISO } from "date-fns";
 import { Link } from "react-router-dom";
 
+interface SightingResponseWrapper {
+  isInternal: boolean;
+  sightingResponse: InternalSightingResponse | ExternalSightingResponse;
+}
+
 export const Sightings: React.FunctionComponent = () => {
   const [internalSightings, setInternalSightings] =
     useState<InternalSightingResponse[]>();
   const [externalSightings, setExternalSightings] =
     useState<ExternalSightingResponse[]>();
 
-  let allSightings: Array<InternalSightingResponse | ExternalSightingResponse> =
-    [];
+  let allSightings: Array<SightingResponseWrapper> = [];
   if (internalSightings !== undefined) {
-    allSightings = allSightings.concat(internalSightings);
+    allSightings = allSightings.concat(
+      internalSightings.map((sighting) => ({
+        isInternal: true,
+        sightingResponse: sighting,
+      }))
+    );
   }
   if (externalSightings !== undefined) {
-    allSightings = allSightings.concat(externalSightings);
+    allSightings = allSightings.concat(
+      externalSightings.map((sighting) => ({
+        isInternal: false,
+        sightingResponse: sighting,
+      }))
+    );
   }
-  allSightings.sort((a, b) => compareDesc(parseISO(a.date), parseISO(b.date)));
+  allSightings.sort((a, b) =>
+    compareDesc(
+      parseISO(a.sightingResponse.date),
+      parseISO(b.sightingResponse.date)
+    )
+  );
 
   useEffect(() => {
     fetchInternalSightings().then((response) =>
@@ -45,9 +64,13 @@ export const Sightings: React.FunctionComponent = () => {
           {allSightings.map((sighting, index) => {
             return (
               <li key={index}>
-                <Link to={`/sightings/${sighting.id}`}>
-                  <SightingCard sighting={sighting} />
-                </Link>
+                {sighting.isInternal ? (
+                  <Link to={`/sightings/${sighting.sightingResponse.id}`}>
+                    <SightingCard sighting={sighting.sightingResponse} />
+                  </Link>
+                ) : (
+                  <SightingCard sighting={sighting.sightingResponse} />
+                )}
               </li>
             );
           })}
